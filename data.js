9cfae919a582585c363d1d6253f4c2df7e53ea6b
KD76V <kvd5006@gmail.com>
2026-01-01 18:07:00 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,35 +1,35 @@
 <script>
-  const DATA = {
-  fightclub:{
-    title:"Fight Club",
-    year:"1999",
-    type:"Movie",
-    poster:"https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg",
-    description:"An insomniac and a soap maker form an underground fight club."
+const DATA = {
+  fightclub: {
+    title: "Fight Club",
+    year: "1999",
+    type: "Movie",
+    poster: "https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg",
+    description: "An insomniac and a soap maker form an underground fight club."
   },
 
-  godfather:{
-    title:"The Godfather",
-    year:"1972",
-    type:"Movie",
-    poster:"https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
-    description:"The aging patriarch of an organized crime dynasty transfers control to his son."
+  godfather: {
+    title: "The Godfather",
+    year: "1972",
+    type: "Movie",
+    poster: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+    description: "The aging patriarch of an organized crime dynasty transfers control to his son."
   },
 
-  breakingbad:{
-    title:"Breaking Bad",
-    year:"2008–2013",
-    type:"Series",
-    poster:"https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg",
-    description:"A chemistry teacher turns into a meth kingpin."
+  breakingbad: {
+    title: "Breaking Bad",
+    year: "2008–2013",
+    type: "Series",
+    poster: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg",
+    description: "A chemistry teacher turns into a meth kingpin."
   },
 
-  aot:{
-    title:"Attack on Titan",
-    year:"2013–2023",
-    type:"Anime",
-    poster:"https://image.tmdb.org/t/p/w500/hTP1DtLGFamjfu8WqjnuQdP1n4i.jpg",
-    description:"Humanity fights for survival against giant Titans."
+  aot: {
+    title: "Attack on Titan",
+    year: "2013–2023",
+    type: "Anime",
+    poster: "https://image.tmdb.org/t/p/w500/hTP1DtLGFamjfu8WqjnuQdP1n4i.jpg",
+    description: "Humanity fights for survival against giant Titans."
   }
 };
 </script>
