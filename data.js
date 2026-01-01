7d2bcf5e6a6c0d81c122cb5a1d44abd56af1b88b
KD76V <kvd5006@gmail.com>
2026-01-01 15:00:01 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,52 @@
+const CONTENT = {
+  "fight-club": {
+    title: "Fight Club",
+    year: "1999",
+    type: "Movie",
+    genre: "Psychological / Drama",
+    director: "David Fincher",
+    poster: "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg",
+    backdrop: "https://wallpapercave.com/wp/wp1817971.jpg",
+    trailer: "https://youtu.be/qtRKdVHc-cE",
+    plot: "An insomniac office worker and a soap maker form an underground fight club that evolves into something much more dangerous.",
+    cast: ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"]
+  },
+
+ godfather: {
+    title: "The Godfather",
+    year: "1972",
+    type: "Movie",
+    genre: "Crime",
+    director: "Francis Ford Coppola",
+    poster: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+    backdrop: "https://image.tmdb.org/t/p/original/rSPw7tgCH9c6NqICZef4kZjFOQ5.jpg",
+    trailer: "https://youtu.be/UaVTIH8mujA",
+    plot: "The aging patriarch of an organized crime dynasty transfers control to his reluctant son.",
+    cast: ["Marlon Brando", "Al Pacino", "James Caan"]
+
+  "aot": {
+    title: "Attack on Titan",
+    year: "2013",
+    type: "Anime",
+    genre: "Dark / War / Fantasy",
+    director: "Hajime Isayama",
+    poster: "https://image.tmdb.org/t/p/w500/hTP1DtLGFamjfu8WqjnuQdP1n4i.jpg",
+    backdrop: "https://wallpapercave.com/wp/wp8238455.jpg",
+    trailer: "https://youtu.be/MGRm4IzK1SQ",
+    plot: "Humanity fights for survival against giant humanoid Titans threatening their extinction.",
+    cast: ["Eren Yeager", "Mikasa Ackerman", "Levi Ackerman"]
+  },
+
+  "breaking-bad": {
+    title: "Breaking Bad",
+    year: "2008",
+    type: "Series",
+    genre: "Crime / Drama",
+    director: "Vince Gilligan",
+    poster: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg",
+    backdrop: "https://wallpapercave.com/wp/wp1939552.jpg",
+    trailer: "https://youtu.be/HhesaQXLuRY",
+    plot: "A chemistry teacher turns to making meth to secure his family's future.",
+    cast: ["Walter White", "Jesse Pinkman", "Saul Goodman"]
+  }
+};
