a898d5c612972099fb6b0637e6d767f3782d5ebf
KD76V <kvd5006@gmail.com>
2026-01-01 18:18:43 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,35 +1,69 @@
-<script>
-const DATA = {
-  fightclub: {
+const CONTENT = {
+  "fight-club": {
     title: "Fight Club",
     year: "1999",
     type: "Movie",
-    poster: "https://image.tmdb.org/t/p/w500/a26cQPRhJPX6GbWfQbvZdrrp9j9.jpg",
-    description: "An insomniac and a soap maker form an underground fight club."
+    genre: "Psychological / Drama",
+    director: "David Fincher",
+    poster: "https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg",
+    backdrop: "https://wallpapercave.com/wp/wp1817971.jpg",
+    trailer: "https://youtu.be/qtRKdVHc-cE",
+    plot: "An insomniac office worker and a soap maker form an underground fight club that evolves into something much more dangerous.",
+    cast: [
+      {name:"Brad Pitt", photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Brad_Pitt_2019.jpg/220px-Brad_Pitt_2019.jpg"},
+      {name:"Edward Norton", photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Edward_Norton_2008.jpg/220px-Edward_Norton_2008.jpg"},
+      {name:"Helena Bonham Carter", photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Helena_Bonham_Carter_2011.jpg/220px-Helena_Bonham_Carter_2011.jpg"}
+    ]
   },
 
-  godfather: {
+  "godfather": {
     title: "The Godfather",
     year: "1972",
     type: "Movie",
+    genre: "Crime / Drama",
+    director: "Francis Ford Coppola",
     poster: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
-    description: "The aging patriarch of an organized crime dynasty transfers control to his son."
+    backdrop: "https://wallpapercave.com/wp/wp1817969.jpg",
+    trailer: "https://youtu.be/UaVTIH8mujA",
+    plot: "The aging patriarch of an organized crime dynasty transfers control to his reluctant son.",
+    cast: [
+      {name:"Marlon Brando", photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Marlon_Brando_1960.jpg/220px-Marlon_Brando_1960.jpg"},
+      {name:"Al Pacino", photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Al_Pacino_2012.jpg/220px-Al_Pacino_2012.jpg"},
+      {name:"James Caan", photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/James_Caan_2011.jpg/220px-James_Caan_2011.jpg"}
+    ]
   },
 
-  breakingbad: {
+  "breaking-bad": {
     title: "Breaking Bad",
     year: "2008–2013",
     type: "Series",
+    genre: "Crime / Drama",
+    director: "Vince Gilligan",
     poster: "https://image.tmdb.org/t/p/w500/ggFHVNu6YYI5L9pCfOacjizRGt.jpg",
-    description: "A chemistry teacher turns into a meth kingpin."
+    backdrop: "https://wallpapercave.com/wp/wp1939552.jpg",
+    trailer: "https://youtu.be/HhesaQXLuRY",
+    plot: "A chemistry teacher turns to making meth to secure his family's future.",
+    cast: [
+      {name:"Walter White", photo:"https://upload.wikimedia.org/wikipedia/en/6/65/Walter_White_S5B.png"},
+      {name:"Jesse Pinkman", photo:"https://upload.wikimedia.org/wikipedia/en/0/03/Jesse_Pinkman_S5B.png"},
+      {name:"Saul Goodman", photo:"https://upload.wikimedia.org/wikipedia/en/7/7c/Saul_Goodman_BCS.png"}
+    ]
   },
 
-  aot: {
+  "aot": {
     title: "Attack on Titan",
     year: "2013–2023",
     type: "Anime",
+    genre: "Dark / War / Fantasy",
+    director: "Hajime Isayama",
     poster: "https://image.tmdb.org/t/p/w500/hTP1DtLGFamjfu8WqjnuQdP1n4i.jpg",
-    description: "Humanity fights for survival against giant Titans."
+    backdrop: "https://wallpapercave.com/wp/wp8238455.jpg",
+    trailer: "https://youtu.be/MGRm4IzK1SQ",
+    plot: "Humanity fights for survival against giant humanoid Titans threatening their extinction.",
+    cast: [
+      {name:"Eren Yeager", photo:"https://upload.wikimedia.org/wikipedia/en/c/c2/Eren_Yeager.png"},
+      {name:"Mikasa Ackerman", photo:"https://upload.wikimedia.org/wikipedia/en/f/f1/Mikasa_Ackerman.png"},
+      {name:"Levi Ackerman", photo:"https://upload.wikimedia.org/wikipedia/en/1/1b/Levi_Ackerman.png"}
+    ]
   }
 };
-</script>
